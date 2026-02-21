948de0c982b2275c3dabeb17dc0624616c854263
RishavKumarMiishra <rishav.kumar@bounteous.com>
2026-02-21 22:28:36 +0530

    Time: 48 ms (56.25%), Space: 44.1 MB (92.61%) - LeetHub

--- 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.java
+++ 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.java
@@ -2,32 +2,26 @@ class Solution {
     public int almostPalindromic(String s) {
         int ans = 0;
         int len = s.length();
-        int[][] dp = new int[len][len];
-        for (int i = 0; i < len; i++) {
-            Arrays.fill(dp[i], -1);
-        }
 
         for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                int l = j-i+1;
-                if (l >= 2 && solve(i, j, dp, s) <= 1) {
-                    ans = Math.max(ans, l);
-                }
-            }
+           ans = Math.max(ans,Math.max(solve(i, i, s, 0), solve(i, i+1, s, 0)));
         }
 
         return ans;
     }
 
-    public int solve(int l, int r, int[][] dp, String s) {
-        if (l >= r) return 0;
-
-        if (dp[l][r] != -1) return dp[l][r];
+    public int solve(int l, int r, String s, int skip) {
+        if (r >= s.length() && l < 0) {
+            return 0;
+        }
+        if (r >= s.length() || l < 0) {
+            return skip == 0 ? 1: 0;
+        }
 
         if (s.charAt(l) == s.charAt(r)) {
-            return dp[l][r] = solve(l+1, r-1, dp, s);
+            return l==r ? 1+solve(l-1, r+1, s, skip) : 2+solve(l-1, r+1, s, skip);
         } else {
-            return dp[l][r] = 1+Math.min(solve(l+1, r, dp, s), solve(l, r-1, dp, s));
+            return skip == 0 ? 1+Math.max(solve(l-1, r, s, 1), solve(l, r+1, s, 1)) : 0;
         }
     }
 }
