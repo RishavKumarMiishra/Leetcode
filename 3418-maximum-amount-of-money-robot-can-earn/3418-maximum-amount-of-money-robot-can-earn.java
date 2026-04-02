2ed83becdc1003b06711716f6a4a0e0a1352a614
RishavKumarMiishra <rishav.kumar@bounteous.com>
2026-04-02 16:36:08 +0530

    Time: 91 ms (82.02%), Space: 238.5 MB (42.7%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
@@ -1,27 +1,47 @@
 class Solution {
-    Integer[][][] memo;
     public int maximumAmount(int[][] coins) {
         int n = coins.length;
         int m = coins[0].length;
-        memo = new Integer[n][m][3];
-        return findMax(coins, 0, 0, 0);
-    }
+        // dp[row][col][negUsed]
+        int[][][] dp = new int[n][m][3];
+        int INF = (int)1e9;
 
-    public int findMax(int[][] coins, int i, int j, int negUsed) {
-        if (i >= coins.length || j >= coins[0].length) return Integer.MIN_VALUE;
-        if (i == coins.length -1 && j == coins[0].length-1) {
-            if (negUsed < 2 && coins[i][j] < 0) return 0;
-            return coins[i][j]; 
+        // Fill from bottom-right to top-left
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                for (int k = 0; k < 3; k++) {
+                    
+                    // BASE CASE: Bottom-Right cell
+                    if (i == n - 1 && j == m - 1) {
+                        int current = coins[i][j];
+                        int takeAsIs = current;
+                        int neutralize = (current < 0 && k < 2) ? 0 : -INF;
+                        dp[i][j][k] = Math.max(takeAsIs, neutralize);
+                        continue;
+                    }
+
+                    // 1. Calculate best path coming from Right or Down
+                    int nextBest = -INF;
+                    if (i + 1 < n) nextBest = Math.max(nextBest, dp[i + 1][j][k]);
+                    if (j + 1 < m) nextBest = Math.max(nextBest, dp[i][j + 1][k]);
+
+                    // Option A: Take the coin normally
+                    int res = coins[i][j] + nextBest;
+
+                    // Option B: Neutralize (if negative and have powers left)
+                    if (coins[i][j] < 0 && k < 2) {
+                        int nextBestWithPowerUsed = -INF;
+                        if (i + 1 < n) nextBestWithPowerUsed = Math.max(nextBestWithPowerUsed, dp[i + 1][j][k + 1]);
+                        if (j + 1 < m) nextBestWithPowerUsed = Math.max(nextBestWithPowerUsed, dp[i][j + 1][k + 1]);
+                        
+                        res = Math.max(res, 0 + nextBestWithPowerUsed);
+                    }
+                    
+                    dp[i][j][k] = res;
+                }
+            }
         }
 
-        if (memo[i][j][negUsed] != null) return memo[i][j][negUsed];
-
-        int take = coins[i][j] + Math.max(findMax(coins, i+1, j, negUsed), findMax(coins, i, j+1, negUsed));
-
-        int neutralize = Integer.MIN_VALUE;
-        if (coins[i][j] < 0 && negUsed < 2) {
-            neutralize = Math.max(findMax(coins, i+1, j, negUsed+1), findMax(coins, i, j+1, negUsed+1));
-        }
-        return memo[i][j][negUsed] = Math.max(take, neutralize);
+        return dp[0][0][0];
     }
 }
